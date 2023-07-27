96cc96ef2ccf8cb056b50f2e9fb1ea14adcd5e77
S10221847 <s10221847@connect.np.edu.sg>
2023-07-28 00:11:19 +0800

    yay streamlit app

--- streamlit_app_group3.py
+++ streamlit_app_group3.py
@@ -1,2 +1,5 @@
 import streamlit as st
-tabs = st.tabs(["tab1", "tab2", "tab3",'tab4','tab5'])
+tab1,tab2,tab3,tab4,tab5 = st.tabs(["tab1", "tab2", "tab3",'tab4','tab5'])
+with tab1:
+    st.header('omg tab1 works yay')
+    
