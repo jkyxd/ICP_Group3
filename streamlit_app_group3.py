1b834bb07c4ca7cf41c13217b0aeee6fa7313d83
jkyxd <127172478+jkyxd@users.noreply.github.com>
2023-07-31 04:28:26 +0800

    Update streamlit_app_group3.py

--- streamlit_app_group3.py
+++ streamlit_app_group3.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+#import plotly.express as px
 st.title('SpeedyBytes 🚚')
 # st.image('speedybytes_icon2.jpg',width=600)
 tab1,tab2,tab3,tab4,tab5 = st.tabs(["tab1", "One year revenue forecast", "Optimal Shift Timing Recommendation",'tab4','tab5'])
