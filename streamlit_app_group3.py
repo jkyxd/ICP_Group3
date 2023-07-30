a91c6a76fac3c696ea97e3b1004663cc1083e84e
jkyxd <127172478+jkyxd@users.noreply.github.com>
2023-07-31 00:23:06 +0800

    Update streamlit_app_group3.py

--- streamlit_app_group3.py
+++ streamlit_app_group3.py
@@ -1,5 +1,16 @@
 import streamlit as st
 tab1,tab2,tab3,tab4,tab5 = st.tabs(["tab1", "tab2", "tab3",'tab4','tab5'])
-with tab1:
+with tab1: #ayrton
     st.header('omg tab1 works yay')
+
+
+with tab2: #minh?
+
+with tab3: #javier
+
+
+
+with tab4: #vibu
+
+with tab5: #nathan
     
