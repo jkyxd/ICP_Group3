dac0b9f8faae02eb20cea416f99f9c749ce2b1c4
vibu <82097715+IndianGrandpa@users.noreply.github.com>
2023-07-31 02:30:28 +0800

    started on my tab

--- streamlit_app_group3.py
+++ streamlit_app_group3.py
@@ -1,6 +1,8 @@
 import streamlit as st
+import pandas as pd
+import plotly.express as px
 st.title('SpeedyBytes 🚚')
-st.image('speedybytes_icon2.jpg',width=600)
+# st.image('speedybytes_icon2.jpg',width=600)
 tab1,tab2,tab3,tab4,tab5 = st.tabs(["tab1", "One year revenue forecast", "Optimal Shift Timing Recommendation",'tab4','tab5'])
 with tab1: #ayrton
     st.header('omg tab1 works yay')
@@ -23,9 +25,60 @@ with tab3: #javier
 
 
 
-with tab4: #vibu
-    print('vibu')
-
-with tab5: #nathan
+with tab4: #natha
     print('nathan')
+
+with tab5: #vibu
+    
+    df= pd.read_csv('truck_location_df.csv')
+    df['Date'] = pd.to_datetime(df['Date'])
+    df["Date"]=pd.to_datetime(df['Date'], format='%d/%m/%Y')
+
+# Sort the DataFrame by Date
+    df.sort_values(by='Date', inplace=True)
+
+# Create the Streamlit app
+    st.set_page_config(
+    page_title="Food Truck Competitor Analysis Dashboard",
+    page_icon="🚚",
+    layout="wide",
+)
+
+# Dashboard title
+    st.title("Food Truck Competitor Analysis Dashboard")
+
+# Overview Section
+    st.header("Overview")
+    df['predicted_earning'] = df['predicted_earning'].apply(lambda x: [int(float(i)) for i in x.strip('[]').split(',')])
+    total_sales=df['predicted_earning'].apply(lambda x: sum(x)).sum()
+
+    average_predicted_earnings = df['predicted_earning'].apply(lambda x: sum(x) / len(x)).mean()
+    total_locations_visited = df['Num_of_locs'].sum()
+
+    st.write(f"Total Sales (Last 2 weeks): ${round(total_sales, 2)}")
+    st.write(f"Average Predicted Earnings: ${round(average_predicted_earnings, 2)}")
+    st.write(f"Total Locations Visited: {total_locations_visited}")
+
+# Sales Performance Section
+    st.header("Sales Performance")
+    fig_sales = px.bar(df, x='Date', y='predicted_earning', title='Predicted Earnings Over Time')
+    st.plotly_chart(fig_sales)
+
+# Efficiency Metrics Section
+    st.header("Efficiency Metrics")
+    fig_hours = px.pie(df, names='Truck_ID', values='working_hour', title='Distribution of Working Hours')
+    fig_shifts = px.bar(df, x='Truck_ID', y='Num_of_locs', title='Number of Locations Visited')
+    st.plotly_chart(fig_hours)
+    st.plotly_chart(fig_shifts)
+
+
+# Prioritization Analysis Section
+    st.header("Prioritization Analysis")
+    df['Priority_Order'] = df['Truck_ID'].rank(method='first')
+    fig_priority = px.bar(df, x='Truck_ID', y='predicted_earning', color='Priority_Order',
+                      labels={'Truck_ID': 'Truck ID', 'predicted_earning': 'Predicted Earnings'},
+                      title='Truck Prioritization Based on Sales Performance')
+    st.plotly_chart(fig_priority)
+
+ 
     
