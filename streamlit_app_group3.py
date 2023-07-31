9c6503dc9a619a15a00c507fd134d14b3ddb03bb
jkyxd <127172478+jkyxd@users.noreply.github.com>
2023-07-31 16:17:23 +0800

    Update streamlit_app_group3.py

--- streamlit_app_group3.py
+++ streamlit_app_group3.py
@@ -16,8 +16,8 @@ with tab3: #javier
     st.header('Optimal Shift Timing Recommendation')
     st.subheader('Want to find out the optimal working hours for your truck?')
     st.subheader('1. Specify your truck details')
-     truck_ids = [27,43,28,44,46,47]
-     truck_id = st.selectbox("Select your Truck ID", truck_ids)
+    truck_ids = [27,43,28,44,46,47]
+    truck_id = st.selectbox("Select your Truck ID", truck_ids)
     if truck_id:
             st.success(f"Your selected Truck ID '{truck_id}' has been saved!")
     st.subheader('2. Specify the number of hours your truck is working for')
