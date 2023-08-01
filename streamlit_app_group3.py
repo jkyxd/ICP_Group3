e56c86062a054cb1ea5d967dbfa8720fc0a8bb44
Vibu <82097715+IndianGrandpa@users.noreply.github.com>
2023-08-02 00:15:44 +0800

    Update streamlit_app_group3.py

--- streamlit_app_group3.py
+++ streamlit_app_group3.py
@@ -156,14 +156,14 @@ with tab3: #javier
             
             city = truck_df['PRIMARY_CITY'].iloc[0]
         
-#             # query = "SELECT * FROM LOCATION WHERE CITY = '{}'".format(city)
-#             # location_df=session.sql(query).toPandas()
-#             location_df=pd.read_csv('location_df.csv')
-#             #location_df = location_df[location_df['CITY']==city]
-#             city_locations = location_df.merge(df_unique_locations_lat_long, left_on='LOCATION_ID', right_on='Location ID', how='inner')
-#             city_locations = city_locations[['LOCATION_ID','Latitude','Longitude']]
-#             city_locations.rename(columns={"Latitude": "LAT"},inplace=True)
-#             city_locations.rename(columns={"Longitude": "LONG"},inplace=True)
+            # query = "SELECT * FROM LOCATION WHERE CITY = '{}'".format(city)
+            # location_df=session.sql(query).toPandas()
+            location_df=pd.read_csv('location_df.csv')
+            #location_df = location_df[location_df['CITY']==city]
+            city_locations = location_df.merge(df_unique_locations_lat_long, left_on='LOCATION_ID', right_on='Location ID', how='inner')
+            city_locations = city_locations[['LOCATION_ID','Latitude','Longitude']]
+            city_locations.rename(columns={"Latitude": "LAT"},inplace=True)
+            city_locations.rename(columns={"Longitude": "LONG"},inplace=True)
         
 #             loc_checker = city_locations.copy()
 #             loc_checker['DATE'] = date
