3a419b0d1fe46d1c46ed6f383a809ea787da4edc
Vibu <82097715+IndianGrandpa@users.noreply.github.com>
2023-08-02 00:06:44 +0800

    Update streamlit_app_group3.py

--- streamlit_app_group3.py
+++ streamlit_app_group3.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import pandas as pd
 import joblib
-# from snowflake.snowpark.session import Session
-# import snowflake.snowpark.functions as F
-# import snowflake.snowpark.types as T
-# from snowflake.snowpark.window import Window
+from snowflake.snowpark.session import Session
+import snowflake.snowpark.functions as F
+import snowflake.snowpark.types as T
+from snowflake.snowpark.window import Window
 # #from sklearn import preprocessing # https://github.com/Snowflake-Labs/snowpark-python-demos/tree/main/sp4py_utilities
 # from snowflake.snowpark.functions import col
 # import plotly.express as px
